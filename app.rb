ef81c8832096098b86b59eef5c36407227194964
icy-wolf <nicole.lawrence93@gmail.com>
2015-01-26 13:33:35 -0500

    setting up project 3

--- app.rb
+++ app.rb
@@ -2,9 +2,7 @@
 require 'sinatra'
 
 class MyWebApp < Sinatra::Base
-  #get '/' do
-  #send_file "views/index#{ rand(3) + 1 }.html"
   get '/' do
-  send_file "views/index.html"
+  send_file "views/index#{ rand(3) + 1 }.html"
 end
 end
