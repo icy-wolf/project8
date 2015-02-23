53324f8c08e9db172b16ea51d7e8cdd5531a8fec
icy-wolf <nicole.lawrence93@gmail.com>
2015-02-23 11:38:06 -0500

    Completed project8

--- app.rb
+++ app.rb
@@ -4,9 +4,15 @@ require 'holidapi'
 
 class HolidaIndustriesApp < Sinatra::Base
   get '/' do
-  	t = Time.now
+  	puts params
+  	if params['select_date'].nil?
+  		t = Time.now
+    	@day = HolidApi.get(country: 'us', year: t.year, month: t.month)
+    else
+    	date = params['select_date'].split("-")
+    	@day = HolidApi.get(country: params['country'], year: date[0], month: date[1])
+    end
     @holidays = HolidApi.get(country: 'us', year: 1993, month: 8)
-    @day = HolidApi.get(country: 'us', year: t.year, month: t.month)
     erb :index
   end
 end
