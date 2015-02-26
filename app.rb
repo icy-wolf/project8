cf8a7fcaaf34180d95a813183a14fb364361bbad
Greenjam94 <Greenjam94@gmail.com>
2015-02-25 23:09:25 -0500

    Index saves params into form and if a day is selected, only that day's holiday is shown

--- app.rb
+++ app.rb
@@ -6,7 +6,7 @@ class HolidaIndustriesApp < Sinatra::Base
   get '/' do
   	puts params
 
-    #if date is NULL
+    #if date is NULL show current month
   	if params['select_date'].nil?
   		t = Time.now
     	@day = HolidApi.get(country: 'us', year: t.year, month: t.month)
@@ -15,19 +15,24 @@ class HolidaIndustriesApp < Sinatra::Base
     	date = params['select_date'].split("/")
       #if country is NOT null
       if ['BE', 'BR', 'GB', 'US'].include? params['country']
-        #if day is set
-        @day = HolidApi.get(country: params['country'], year: date[2], month: date[0], day: date[1])
-        #else day not set
-          #call for whole month
+        if date[1].empty? || date[1].nil?
+          @day = HolidApi.get(country: params['country'], year: date[2], month: date[0])
+        else
+          @day = HolidApi.get(country: params['country'], year: date[2], month: date[0], day: date[1])
+        end
       #else assume 'Merica
       else
-        #if day set
-        @day = HolidApi.get(country: 'US', year: date[2], month: date[0], day: date[1])
-        #else no day
-          #call for month
+        if date[1].empty? || date[1].nil?
+          @day = HolidApi.get(country: 'US', year: date[2], month: date[0])
+        else
+          @day = HolidApi.get(country: 'US', year: date[2], month: date[0], day: date[1])
+        end
       end
     end
+
+    #Nicole's month of Birth
     @holidays = HolidApi.get(country: 'US', year: 1993, month: 8)
+    
     erb :index
   end
 end
